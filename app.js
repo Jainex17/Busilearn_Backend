344c4045eb765524a13c2e88814b855e575f2b4f
jainex17 <jainexp017@gmail.com>
2023-02-18 18:58:03 +0530

    fixxxx deployment 10

--- app.js
+++ app.js
@@ -1,47 +1,34 @@
-// const express = require("express");
-// const app = express();
-// const cookieParser = require('cookie-parser')
-// const cors = require('cors');
-
-// const errorMiddleware = require('./middleware/error');
-
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cors({
-//     origin: process.env.FRONTEND_URL,
-//     credentials: true,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    
-// }));
-
-// // route imports
-// const courseroute = require("./routes/CourseRoute")
-// const userroute = require("./routes/UserRoute")
-
-// app.get("/", (req, res) => { 
-//     res.send(`server is working frontend in`); 
-// });
-
-// app.use("/api/v1",courseroute);
-// app.use("/api/v1",userroute);
-
-
-// // Middleware for errors
-// app.use(errorMiddleware);
-
-// module.exports = app
-
 const express = require("express");
 const app = express();
+const cookieParser = require('cookie-parser')
+const cors = require('cors');
+
+const errorMiddleware = require('./middleware/error');
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    
+}));
+
+// route imports
+const courseroute = require("./routes/CourseRoute")
+const userroute = require("./routes/UserRoute")
 
 app.get("/", (req, res) => { 
-    res.send(`server is working frontend in ${process.env.FRONTEND_URL}`); 
+    res.send(`server is working frontend in`); 
 });
 
+app.use("/api/v1",courseroute);
+app.use("/api/v1",userroute);
 
+
+// Middleware for errors
+app.use(errorMiddleware);
 
 module.exports = app
+
