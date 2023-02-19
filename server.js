89366d55e31bfa237bcf1854d25dd6d21825080a
jainex17 <jainexp017@gmail.com>
2023-02-19 13:54:21 +0530

    fixxxx deployment 11

--- server.js
+++ server.js
@@ -1,31 +1,28 @@
 const app = require('./app.js');
 const dotenv = require('dotenv');
-const connectDatabase = require("./config/database")
-const cloudinary = require("cloudinary")
+// const connectDatabase = require("./config/database")
+// const cloudinary = require("cloudinary")
 
-// Handling Uncaught exception
-process.on("uncaughtException",err=>{
-    console.log(`error: ${err.message}`);
+// // Handling Uncaught exception
+// process.on("uncaughtException",err=>{
+//     console.log(`error: ${err.message}`);
 
-    console.log("shutting down the server due to Uncaught exception");
-    process.exit(1);
-});
+//     console.log("shutting down the server due to Uncaught exception");
+//     process.exit(1);
+// });
 
 
 // config
 dotenv.config({path:"config.env"});
 
-// connect database
-connectDatabase();
+// // connect database
+// connectDatabase();
 
-cloudinary.v2.config({
-    cloud_name:process.env.CLOUDINARY_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_API_SECRET,
-});
-
-
-
+// cloudinary.v2.config({
+//     cloud_name:process.env.CLOUDINARY_NAME,
+//     api_key:process.env.CLOUDINARY_API_KEY,
+//     api_secret:process.env.CLOUDINARY_API_SECRET,
+// });
 
 
 
@@ -34,11 +31,11 @@ const server = app.listen(8000,()=>{
 })
 
 // unhandled Promise Rejection
-process.on('unhandledRejection',err=>{
-    console.log(`error: ${err.message}`);
-    console.log("shutting down the server due to unhadled promise rejection");
+// process.on('unhandledRejection',err=>{
+//     console.log(`error: ${err.message}`);
+//     console.log("shutting down the server due to unhadled promise rejection");
 
-    server.close(()=>{
-        process.exit(1);
-    });
-});
+//     server.close(()=>{
+//         process.exit(1);
+//     });
+// });
