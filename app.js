def28a40b36220ea58b57e5d6eeb7b0bc45056db
jainex17 <jainexp017@gmail.com>
2023-02-20 20:51:11 +0530

    env config moved in app.js

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
 const express = require("express");
 const app = express();
-// const cookieParser = require('cookie-parser')
-// const cors = require('cors');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser')
+const cors = require('cors');
 
-// const errorMiddleware = require('./middleware/error');
+const errorMiddleware = require('./middleware/error');
+
+
+// config for env variables
+dotenv.config({path:"config.env"});
+
 
 app.use(express.json());
-// app.use(cookieParser());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors({
-//     origin: process.env.FRONTEND_URL,
-//     credentials: true,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    
-// }));
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    optionSuccessStatus:200,
+
+}));
 
 // route imports
-// const courseroute = require("./routes/CourseRoute")
-// const userroute = require("./routes/UserRoute")
+const courseroute = require("./routes/CourseRoute")
+const userroute = require("./routes/UserRoute")
 
 app.get("/", (req, res) => { 
     res.send(`server is working frontend in`); 
 });
 
-// app.use("/api/v1",courseroute);
-// app.use("/api/v1",userroute);
+app.use("/api/v1",courseroute);
+app.use("/api/v1",userroute);
 
 
 // Middleware for errors
-// app.use(errorMiddleware);
+app.use(errorMiddleware);
 
 module.exports = app
-
--- server.js
+++ server.js
@@ -1,41 +1,35 @@
 const app = require('./app.js');
-const dotenv = require('dotenv');
-// const connectDatabase = require("./config/database")
-// const cloudinary = require("cloudinary")
+const connectDatabase = require("./config/database")
+const cloudinary = require("cloudinary")
 
-// // Handling Uncaught exception
-// process.on("uncaughtException",err=>{
-//     console.log(`error: ${err.message}`);
+// Handling Uncaught exception
+process.on("uncaughtException",err=>{
+    console.log(`error: ${err.message}`);
 
-//     console.log("shutting down the server due to Uncaught exception");
-//     process.exit(1);
-// });
+    console.log("shutting down the server due to Uncaught exception");
+    process.exit(1);
+});
 
 
-// config
-dotenv.config({path:"config.env"});
+// connect database
+connectDatabase();
 
-// // connect database
-// connectDatabase();
-
-// cloudinary.v2.config({
-//     cloud_name:process.env.CLOUDINARY_NAME,
-//     api_key:process.env.CLOUDINARY_API_KEY,
-//     api_secret:process.env.CLOUDINARY_API_SECRET,
-// });
-
-console.log("runing");
+cloudinary.v2.config({
+    cloud_name:process.env.CLOUDINARY_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
+});
 
 const server = app.listen(8000,()=>{
     console.log('server runing on 8000');
 })
 
 // unhandled Promise Rejection
-// process.on('unhandledRejection',err=>{
-//     console.log(`error: ${err.message}`);
-//     console.log("shutting down the server due to unhadled promise rejection");
+process.on('unhandledRejection',err=>{
+    console.log(`error: ${err.message}`);
+    console.log("shutting down the server due to unhadled promise rejection");
 
-//     server.close(()=>{
-//         process.exit(1);
-//     });
-// });
+    server.close(()=>{
+        process.exit(1);
+    });
+});
